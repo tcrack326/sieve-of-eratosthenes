e55425aa1a244306d58c3a6447df21926514bd76
Thomas McCracken <thomasmccracken@Thomass-MacBook-Pro.local>
2023-04-27 12:27:40 -0400

    initial commit to remote

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import { useState, useEffect } from'react';
 import './App.css';
-
+ 
+/** SIEVE OF ERATOSTHENES
+* For referencce, https://www.geeksforgeeks.org/sieve-of-eratosthenes/
+*/
+ 
+function createRange(size) {
+  const range = Array.from({ length: +size + 1}, (val, idx) => idx).filter(num => num !== 0 && num !== 1)
+ 
+  return range
+}
+ 
 function App() {
+  /* TODO: IMPLEMENT CODE HERE */
+ 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       Sieve of Eratosthenes
       </header>
+      <form onSubmit={/*TODO: IMPLEMENT */}>
+        <input disabled={/*TODO: IMPLEMENT */} name="maxprime" type="number" />
+        <button type="submit">Find Primes</button>
+      </form>
+      {/*TODO: IMPLEMENT LIST OF PRIMES AND NON-PRIMES */}
     </div>
   );
 }
-
+ 
 export default App;
